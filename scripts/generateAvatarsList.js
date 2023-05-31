49136999019145ce78fa2fb7bb7473168251c3ad
Faiyaz Shaikh <www.shtakkar@gmail.com>
2023-05-31 10:42:27 +0530

    feat: add base url

--- scripts/generateAvatarsList.js
+++ scripts/generateAvatarsList.js
@@ -2,7 +2,11 @@ const glob = require("glob");
 const path = require("path");
 const fs = require("fs");
 
+const BASE_URL = 'https://avatars.mylikes.page'
+
 const avatars = glob.sync("./public/avatars/**/*.svg");
-const formattedAvatars = avatars.map((avatar) => avatar.replace('public', ''))
+const formattedAvatars = avatars.map((avatar) => {
+  return avatar.replace('public', BASE_URL)
+})
 
 fs.writeFileSync(path.join(process.cwd(), "public/avatars.json"), JSON.stringify(formattedAvatars, null, 2))
